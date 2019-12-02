86895f90f5d9a9835b735e51ad01bb5940676bd4
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2019-12-02 18:46:19 -0500

    adds more

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class Partner < ApplicationRecord
+  enum record_status: { active: 0, inactive: 1, deleted: 2 }
 end
--- app/models/registration_form.rb
+++ app/models/registration_form.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class RegistrationForm < ApplicationRecord
   belongs_to :partner
   belongs_to :sport_type
+
+  enum record_status: { active: 0, inactive: 1, deleted: 2 }
 end
